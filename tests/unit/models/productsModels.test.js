e2d2e75a611e172a576a9b682f284f4cfe416484
Andre Moraes <andre.moraes.98@gmail.com>
2022-07-04 19:07:22 -0300

    Conclusão dos testes do productModel

--- tests/unit/models/productsModels.test.js
+++ tests/unit/models/productsModels.test.js
@@ -99,3 +99,88 @@ describe('Retorna o produto criado', () => {
     expect(result).to.be.an('object').with.keys('id', 'name');
   });
 });
+
+describe('Retorna os ids dos produtos', () => {
+  before(() => {
+    const result = [[
+      { id: 1 },
+      { id: 2 },
+      { id: 3 },
+    ]];
+    sinon.stub(connection, 'query').resolves(result);
+  });
+
+  after(() => {
+    connection.query.restore();
+  });
+
+  it('espera que seja retornado um array de objetos com a chave "id".', async () => {
+    const idProducts = await ModelProduct.getIdProducts();
+
+    expect(idProducts).to.be.a('array');
+    idProducts.forEach((idObject) => expect(idObject).to.be.a('object').with.key('id'));
+  });
+});
+
+describe('Atualiza um produto', () => {
+  describe('espera que seja retornado um objeto', () => {
+    before(() => {
+      sinon.stub(connection, 'query');
+    });
+
+    after(() => {
+      connection.query.restore();
+    });
+
+    it('que tenha uma chave "id" e "name".', async () => {
+      const NAME = 'Manopla do Thanos';
+      const ID = '1';
+
+      const result = await ModelProduct.update(NAME, ID);
+
+      expect(result).to.be.a('object').with.keys('id', 'name');
+    });
+  });
+});
+
+describe('Deleta um produto', () => {
+  before(() => {
+    sinon.stub(connection, 'query');
+  });
+
+  after(() => {
+    connection.query.restore();
+  });
+
+  it('espera que um null seja retornado.', async () => {
+    const result = await ModelProduct.deleteById(1);
+
+    expect(result).to.be.null;
+  })
+});
+
+describe('Filtra um produto por um termo', () => {
+  describe('quando chamado com "mart"', () => {
+    before(() => {
+      const result = [[{
+        id: 1,
+        name: 'Martelo do Thor',
+      }]];
+
+      sinon.stub(connection, 'query').resolves(result);
+    });
+
+    after(() => {
+      connection.query.restore();
+    });
+
+    it('espera que seja retornado um objeto com chave "id" e "name".', async () => {
+      const result = await ModelProduct.searchByTerm('mart');
+
+      expect(result).to.be.a('array');
+      expect(result[0]).to.be.a('object').with.keys('id', 'name');
+      expect(result[0].id).to.be.equal(1);
+      expect(result[0].name).to.be.equal('Martelo do Thor');
+    })
+  });
+});
